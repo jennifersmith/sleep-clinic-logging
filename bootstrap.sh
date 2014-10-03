1f95ca1b03fc828587301ecbfb15c0b2e05a932f
jennifersmith <jennifer@home>
2014-10-04 09:15:23 +1000

    Kinda idempotent scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,12 @@
+# postgres setup
+
+#mkdir data/postgres
+
+#postgres - l
+
+# want to make these just idempotent ln but i cant :(
+
+rm -f data/taplog 
+rm -f data/fitbit
 ln -s ~/Dropbox/taplog/ data/taplog
 ln -s ~/Dropbox/data/fitbit/ data/fitbit
