cb30e7aca10b07307a6032fcc7b2701c50b93cdb
jennifersmith <jennifer@home>
2014-10-04 11:22:15 +1000

    Bootstrap starts up a postgres server too

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
+set -e
+
 # postgres setup
 
-#mkdir data/postgres
+(pg_ctl -D conf/ -l logs/postgres-server.log stop)
+rm -rf data/postgres
+initdb data/postgres >> logs/bootstrap.log
+#Note to self : -w waits supposedly for server to start. this appears to work so far. Hard won tho :)
+pg_ctl -w -D conf/ -l logs/postgres-server.log start
 
-#postgres - l
-
+psql postgres -c "create database sleepdiary"
+psql sleepdiary -c "CREATE TABLE events ( id SERIAL, data JSON );"
 # want to make these just idempotent ln but i cant :(
-
 rm -f data/taplog 
 rm -f data/fitbit
 ln -s ~/Dropbox/taplog/ data/taplog
